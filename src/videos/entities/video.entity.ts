6cc8c4074ddf66360d121e15366f6f07cb6f22fa
jiwonemil <jjiwon24@gmail.com>
2023-01-03 10:25:31 -0700

    video.entity 추가 : useId는 관계설정 필요함

--- src/videos/entities/video.entity.ts
+++ src/videos/entities/video.entity.ts
@@ -1,1 +1,45 @@
-export class Video {}
+import {
+  BaseEntity,
+  Column,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+//import { User } from '../../users/entities';
+
+export class Video extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  videoId: number;
+
+  /* @ManyToOne(() => User, (user) => user.videos)
+  user: User; */
+
+  @Column()
+  userId: number;
+
+  @Column()
+  likeNum: number;
+
+  @Column()
+  videoName: string;
+
+  @Column()
+  videoImg: string;
+
+  @Column()
+  videoDest: string;
+
+  @CreateDateColumn()
+  createdAt: Timestamp;
+
+  @Column()
+  views: number;
+
+  @Column()
+  commentNum: number;
+
+  @Column('simple-json')
+  category: { id: number; name: string };
+}
