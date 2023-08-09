06a93171eb2f24cad8a8746ab63a3ee6d32f7666
jiwonjeon <j.jeon339@mybvc.ca>
2023-08-09 16:25:54 -0600

    Add usersmodule into the videomodule

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -4,13 +4,16 @@ import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Video } from './entities/video.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     TypeOrmModule.forFeature([Video]),
     MulterModule.register({ dest: './uploads' }),
   ],
   controllers: [VideosController],
   providers: [VideosService],
+  exports: [VideosService],
 })
 export class VideosModule {}
