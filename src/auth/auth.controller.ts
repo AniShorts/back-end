1dc8b66e1c68108652789d4a7cce221fc5139a11
cesdea <aprkvnem@gmail.com>
2023-08-14 16:11:47 +0900

    add(auth) : 약식 이메일 인증, swagger 작성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { AuthService } from './auth.service';
 import {UsersService} from '../users/users.service'
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { accessTokenType, allTokenType, emailType, numberType, refreshTokenType } from './auth.AnyType';
 
 @Controller('auth')
 export class AuthController {
@@ -13,15 +15,34 @@ export class AuthController {
   /*
   access토큰이 만료될경우 refresh토큰을 refresh로 refresh token을 받아서 access 토큰을 발급받는다.
   */ 
+  /**
+   * Access토큰 재생성
+   * @param req 
+   * @param res 
+   * @returns 
+   */
+  @ApiBody({type:refreshTokenType})
+  @ApiOperation({ summary: 'Access토큰 재생성 API', description: 'access토큰 재발급.' })
+  @ApiResponse({status:200, description: 'access 토큰 제공.', type: accessTokenType })
   @UseGuards(AuthGuard('refresh'))
   @Post()
-  async getAccessToken(@Req() req) {
-      return req.user
+  async getAccessToken(@Req() req:any,@Res() res:Response) {
+    const access:string=req.users
+      return res.send(new accessTokenType(access))
   }
 
+  /**
+   * Resfresh토큰 재생성
+   * @param body 
+   * @returns 
+   */
+  @ApiBody({type:allTokenType})
+  @ApiOperation({ summary: 'Resfresh토큰 재생성 API', description: 'Resfresh토큰 재발급.' })
+  @ApiResponse({status:200, description: 'Resfresh토큰 토큰 제공.', type: refreshTokenType })
   @Post('getRefresh')
-  async getRefreshToken(@Body() body:{access:string,refresh:string}) {
-    return {refresh:await this.authService.getRefreshToken(body.access,body.refresh)}
+  async getRefreshToken(@Body() body:{access:string,refresh:string}, @Res() res:Response) {
+    const refresh:string=await this.authService.getRefreshToken(body.access,body.refresh)
+    return res.send(new refreshTokenType(refresh))
   }
 
 
@@ -30,12 +51,14 @@ export class AuthController {
    * @param body 
    * @param res 
    */
+  @ApiBody({type:emailType})
+  @ApiOperation({ summary: '인증번호용 이메일 전송API', description: '인증번호를 해당 email로 전송' })
+  @ApiResponse({status:200, description: '인증번호용 이메일을 전송한다.', type: numberType })
   @Post('email')
   async sendEmail(@Body() body:any,@Res() res:Response){
-    const randomNum=await this.authService.sendEmail('aa')
+    const {email}=body
+    const randomNum=await this.authService.sendEmail(email)
     
-    return res.send({
-      number:randomNum
-    })
+    return res.send(new numberType(randomNum))
   }
 }
